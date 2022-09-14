0b29964f76bcc8b2beb1a02a5f02cba124e05f1b
Kunal Kadam <kunalpk007@gmail.com>
2022-09-15 03:39:46 +0530

    deploy to heroku

--- src/app.test.js
+++ src/app.test.js
@@ -6,6 +6,6 @@ describe("/test endpoint", ()=>{
     it("should return a response",async ()=>{
         const response = await request.get("/test");
         expect(response.statusCode).toBe(200);
-        expect(response.text).toBe("Success1");
+        expect(response.text).toBe("Success");
     })
 })
