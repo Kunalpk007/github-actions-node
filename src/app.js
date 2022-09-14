3640b36102d45dc2eee3d8490cda32c11ca1144c
Kunal Kadam <kunalpk007@gmail.com>
2022-09-15 04:11:33 +0530

    reverted path

--- src/app.js
+++ src/app.js
@@ -4,5 +4,8 @@ const app = express();
 app.get('/test',(_req,res) =>{
     res.status(200).send("Success");
 });
+app.get('/',(_req,res) =>{
+    res.status(200).send("Get success");
+});
 
 module.exports = app;
